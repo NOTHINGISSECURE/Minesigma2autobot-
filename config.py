ff3f216a374fe6d5825d6b3f0572c13e33613e14
NOTHINGISSECURE <154494591+NOTHINGISSECURE@users.noreply.github.com>
2024-08-29 08:13:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://indiakeedits:<VwN8KejpmFRVjIWw>@cluster0.y0gi9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = environ.get("DB_URI", "mongodb+srv://Cluster0:<MHpDtzlEbadLDHzc>@cluster0.4ancz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "Cluster0")
 
 # Auto Delete Information
